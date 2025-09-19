983aed16ae9eb7c5f969c67723ed8ecc79fe126d
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2025-09-19 15:58:37 +0530

    Delete LEAPYEAR.py

--- LEAPYEAR.py
+++
@@ -1,6 +0,0 @@
-startyr=int(input('Enter the starting year :'))
-endyr=int(input('Enter the ending year :'))
-print('Leap years starting from ', startyr ,'to ending at ',endyr, 'is :')
-for year in range(startyr,endyr + 1):
-    if(year % 4 == 0 and year % 100 != 1) or (year % 400 == 0):
-        print( year, end=' ')
