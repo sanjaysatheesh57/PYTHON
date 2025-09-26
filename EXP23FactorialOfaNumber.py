d2ec6db473c3bd4d4e5347feddf06b67e4f3f0c9
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2025-09-26 10:06:06 +0530

    EXP23FactorialOfaNumber

---
+++ EXP23FactorialOfaNumber.py
@@ -0,0 +1,10 @@
+num= int(input("Enter the number to find factorial: "))
+factorial = 1
+if(num<0):
+    print("Sorry,Factorial does not exist for negative numbers")
+elif(num==0):
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial = factorial * i
+print("The factorial of the given number ",num," is ",factorial)
