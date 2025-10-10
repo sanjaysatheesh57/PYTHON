f7a7e7960925439fe4ed54e5b446e2efe684127a
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2025-10-10 06:27:22 +0100

    FunctionForEvenODD

---
+++ FunctionForEvenODD.py
@@ -0,0 +1,9 @@
+def evenodd(num):
+    if num % 2 == 0:
+        print('Even')
+    else:
+        print('odd')
+    return evenodd
+
+num= int(input('Enter a number to check whether it is even or odd :'))
+n= evenodd(num)
