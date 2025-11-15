a5be0f8371e6164de255441372d6729d8c3c617f
Sanjay Satheesh <sanjaysatheesh66@gmail.com>
2025-11-15 14:41:06 +0530

    Time_operatoroverloading

---
+++ Time_operatoroverloading.py
@@ -0,0 +1,17 @@
+class Time:
+    def __init__(self,hour,minute,second):
+        self.__hour=hour
+        self.__minute=minute
+        self.__second=second
+    def __add__(self,other):
+         new_sec = self.__second + other.__second
+         new_min = self.__minute + other.__minute + new_sec 
+         new_sec = new_sec % 60
+         new_hour = self.__hour + other.__hour + new_min // 60
+         new_min = new_min % 60
+         return Time(new_hour, new_min, new_sec)
+    def show(self):
+        print(f"{self.__hour} hr {self.__minute} min {self.__second} sec")
+t1 = Time(2, 45, 50)
+t2 = Time(3, 20, 30)
+t3 = t1 + t2
